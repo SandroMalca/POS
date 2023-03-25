0a3cc7a20d7441dac3b48bb991edb04eac45dc95
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-03-24 21:10:43 -0300

    contenido de las carpetas

---
+++ components/card/Card.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function Card() {
+  return (
+    <View>
+      <Text>Card</Text>
+    </View>
+  );
+}
+
+export default Card;
---
+++ containers/index.ts
@@ -0,0 +1,3 @@
+export { default as Cart } from './cart/Cart';
+export { default as Home } from './home/Home';
+export { default as Inventory } from './inventory/Inventory';
