0cf7efd4be7a6b5dfadd2234661593b55ab5bea5
Sandro <sandro_malca@usmp.pe>
2023-03-25 19:33:29 -0500

    add Login, Stats

---
+++ components/login/Login.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Login() {
+  return (
+    <View>
+      <Text>¡Bienvenido de nuevo! <br /> Por favor, indentifique para tener acceso</Text>
+      <Text>Login</Text>
+    </View>
+  )
+}
