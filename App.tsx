18425b4941b591dccbbbcba48c6afa786dbc8c32
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-03-30 20:25:49 -0300

    firebase y screens

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { Home, Cart, Inventory, Stats, Config } from './containers';
+import { Home, Cart, Inventory, Stats, Config } from './screens';
 
 const Drawer = createDrawerNavigator()
 
@@ -20,12 +18,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/index.ts
+++ screens/index.ts
@@ -2,4 +2,6 @@ export { default as Cart } from './cart/Cart';
 export { default as Config } from './config/Config';
 export { default as Home } from './home/Home';
 export { default as Inventory } from './inventory/Inventory';
+export { default as addProduct } from './inventory/addProduct/addProduct';
+export { default as editProduct } from './inventory/editProduct/editProduct';
 export { default as Stats } from './stats/Stats';
